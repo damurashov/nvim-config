398c8fde9d6d4798fa0025c0ed74baa0c993aab1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-18 14:19:22 +0300

    [:init] Impl | Telescope find on newtab - disabled, as there is a substitute feature

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ vim.api.nvim_create_user_command("DApteb", "Telescope buffers", {})  -- Plugin T
 vim.api.nvim_create_user_command("DAttg", "Telescope buffers", {})  -- Text TaGs
 vim.api.nvim_set_keymap("n", "-p", ":Telescope find_files <CR>", {})  -- Find files in the current pwd
 vim.api.nvim_set_keymap("n", "-g", ":Telescope current_buffer_fuzzy_find <CR>", {})  -- Search for a sequence in the current file using fuzzy search
-vim.api.nvim_set_keymap("n", "-t", " :lua =win_tab_new() <CR>", {})  -- Open new tab and search for files
+-- vim.api.nvim_set_keymap("n", "-t", " :lua =win_tab_new() <CR>", {})  -- Open new tab and search for files
 
 function win_tab_new()
 	vim.cmd([[
