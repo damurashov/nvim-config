d0210aebb4c47fd4ec0773c38500749810582613
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-18 15:33:12 +0300

    [lua:my_plugins] Ref | Encapsulation

--- lua/my_plugins.lua
+++ lua/my_plugins.lua
@@ -2,15 +2,19 @@
 vim.cmd [[packadd packer.nvim]]
 require("telescope")  -- Enable plugin "Telescope"
 
-local rt = require("rust-tools")
+function config_setup_rust_tools()
+	local rt = require("rust-tools")
 
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
-})
+	rt.setup({
+	  server = {
+		on_attach = function(_, bufnr)
+		  -- Hover actions
+		  vim.keymap.set("i", "-s", rt.hover_actions.hover_actions, { buffer = bufnr })
+		  -- Code action groups
+		  vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
+	  },
+	})
+end
+
+config_setup_rust_tools()
