54bcee2e35b1d8192699a1785007d7549cb8e3af
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-20 10:42:21 +0300

    [:init] Impl | EasyAlign plugin, new shortcuts

--- init.lua
+++ init.lua
@@ -3,14 +3,15 @@ vim.cmd([[
 	syntax on
 	set ignorecase
 	set number
-	colorscheme desert
-	xnoremap :X "+y
+	colorscheme peachpuff
+	vnoremap :Y "+y
 ]])
 vim.g.rust_recommended_style = 0
 vim.g.python_recommended_style = 0
 vim.api.nvim_create_user_command("DAwwide", "vertical resize 120", {})  -- Window Widen
 vim.api.nvim_create_user_command("DAwt", "tab split", {})  -- Window split into Tab
 vim.api.nvim_create_user_command("DAcreload", "source ~/.config/nvim/init.lua", {})  -- Config Reload
+vim.api.nvim_create_user_command("DAcedit", "tabnew ~/.config/nvim/init.lua", {})  -- Config EDIT
 vim.api.nvim_create_user_command("DAttabs", "set noexpandtab shiftwidth=4 tabstop=4", {})  -- Text use Tabs
 vim.api.nvim_create_user_command("DAtspaces", "set expandtab shiftwidth=4 tabstop=4", {})  -- Text use Spaces
 vim.api.nvim_create_user_command("DAptefif", "Telescope find_files", {})  -- Plugin TElescope FInd Files
@@ -18,6 +19,7 @@ vim.api.nvim_create_user_command("DApteb", "Telescope buffers", {})  -- Plugin T
 vim.api.nvim_create_user_command("DAttg", "Telescope buffers", {})  -- Text TaGs
 vim.api.nvim_set_keymap("n", "-p", ":Telescope find_files <CR>", {})  -- Find files in the current pwd
 vim.api.nvim_set_keymap("n", "-g", ":Telescope current_buffer_fuzzy_find <CR>", {})  -- Search for a sequence in the current file using fuzzy search
+vim.api.nvim_set_keymap("n", "-G", ":Telescope live_grep <CR>", {})  -- Search among files using fuzzy search
 vim.api.nvim_set_keymap("n", "-u", ":lua =config_packer_sync() <CR>", {})  -- Synchronize packer
 -- vim.api.nvim_set_keymap("n", "-t", " :lua =win_tab_new() <CR>", {})  -- Open new tab and search for files
 
@@ -26,6 +28,7 @@ function config_packer_sync()
 		function(use)
 			use 'neovim/nvim-lspconfig'
 			use 'simrat39/rust-tools.nvim'
+			use 'junegunn/vim-easy-align'
 		end
 	).sync()
 end
