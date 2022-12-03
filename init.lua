821829432db3cc3b3c7ebc1ee36ff84d1c5cf080
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-03 09:35:53 +0300

    [:init] Impl | "Open in vscode" command

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ vim.g.rust_recommended_style = 0
 vim.g.python_recommended_style = 0
 vim.api.nvim_create_user_command("DAwwide", "vertical resize 120", {})  -- Window Widen
 vim.api.nvim_create_user_command("DAwt", "tab split", {})  -- Window split into Tab
+vim.api.nvim_create_user_command("DAwoc", "!code %", {})  -- Window Open in Code
 vim.api.nvim_create_user_command("DAcreload", "source ~/.config/nvim/init.lua", {})  -- Config Reload
 vim.api.nvim_create_user_command("DAcedit", "tabnew ~/.config/nvim/init.lua", {})  -- Config EDIT
 vim.api.nvim_create_user_command("DAttabs", "set noexpandtab shiftwidth=4 tabstop=4", {})  -- Text use Tabs
